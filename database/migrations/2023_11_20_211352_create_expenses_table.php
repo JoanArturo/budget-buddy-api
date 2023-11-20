ac43e4a34117966bededecd0e6a3c779a28e29f9
JoanArturo <joanarturo81@gmail.com>
2023-11-20 15:24:30 -0600

    feat: add models and migrations

---
+++ database/migrations/2023_11_20_211352_create_expenses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 100);
+            $table->unsignedDecimal('amount', 10, 2);
+            $table->text('description');
+            $table->date('expense_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
